16f2eae6952616a3cbff03cf46cf7ddf8ddd43ae
418899 <115142712+418899@users.noreply.github.com>
2022-10-06 16:41:46 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            if (y % 2 != 0) {
+                led.plot(4 - x, y)
+            } else {
+                led.plot(x, y)
+            }
+            basic.pause(100)
+        }
+    }
+})
